7fb882fa13755316f9ab03a1af863a24d176111c
ibito <180943+ibito@users.noreply.github.com>
2024-02-23 15:50:43 -0700

    Add FontAwesome icons and fix device rotation issue

--- app/components/EmblaCarousel.jsx
+++ app/components/EmblaCarousel.jsx
@@ -1,34 +1,91 @@
-"use client"
-/* eslint-disable @next/next/no-img-element */
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/legacy/image";
+import RotateDevice from "./RotateDevice";
 
 const EmblaCarousel = (props) => {
   const { slides, options } = props;
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
-  // console.log('slides: ', slides)
+
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [screenHeight, setScreenHeight] = useState(0);
+  const [showWarning, setShowWarning] = useState(false);
+
+  // const width = window.screen.width * window.devicePixelRatio;
+  // const height = window.screen.height * window.devicePixelRatio;
+
+  // console.log(width, height)
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.screen.width * window.devicePixelRatio;
+      const height = window.screen.height * window.devicePixelRatio;
+      setScreenWidth(width);
+      setScreenHeight(height);
+
+      // show warning if screen width is less than screen height
+      if (screenWidth <= screenHeight) {
+        console.warn("Please rotate your device for better experience handleResize");
+        setShowWarning(true);
+      } else {
+        setShowWarning(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [screenWidth, screenHeight]);
+
+  useEffect(() => {
+    getScreenSize();
+  }, []);
+
+  const getScreenSize = () => {
+    const width = window.screen.width * window.devicePixelRatio;
+    const height = window.screen.height * window.devicePixelRatio;
+
+    setScreenWidth(width);
+    setScreenHeight(height);
+
+    if (width <= height) {
+      console.warn("Please rotate your device for better experience");
+      setShowWarning(true);
+    } else {
+      setShowWarning(false);
+    }
+  };
 
   return (
     <section className="embla">
       <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
-              <div className="embla__slide__number"  style={{width: '100vw', height: '100vh', position: 'relative'}}>
-                <Image
-                  src={`${index}`}
-                  alt={''}
-                  fill="true"
-                  layout="fill"
-                  objectFit="cover"
-                  placeholder="blur"
-                  blurDataURL={`${index}`}
-                />
+        {showWarning && <RotateDevice />}
+        {!showWarning && (
+          <div className="embla__container">
+            {slides.map((index) => (
+              <div className="embla__slide" key={index}>
+                <div
+                  className="embla__slide__number"
+                  style={{
+                    width: "100vw",
+                    height: "100vh",
+                    position: "relative",
+                  }}
+                >
+                  <Image
+                    src={`${index}`}
+                    alt={""}
+                    fill="true"
+                    layout="fill"
+                    objectFit="cover"
+                    placeholder="blur"
+                    blurDataURL={`${index}`}
+                  />
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,14 @@ import "../css/sandbox.css";
 import "../css/embla.css";
 import fs from "fs";
 
-// console.log(galleryImages.keys())
 const OPTIONS = { align: "start", loop: true };
 const SLIDE_COUNT = 13;
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
-console.log(SLIDES);
 
 // En tu archivo de ruta o controlador del servidor
 export async function getServerSideProps() {
   const folderPath = "./public/gallery"; // Ruta a la carpeta deseada
   const fileNames = fs.readdirSync(folderPath);
-  console.log("fileNames: ", fileNames);
 
   const finalFileNames = fileNames.map((fileName) => {
     return `/gallery/${fileName}`;
